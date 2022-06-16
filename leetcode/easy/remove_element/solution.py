12e38c27358ddad11475772e355b01c48f217a5f
YernarT <toktaryernar@gmail.com>
2022-06-16 23:43:03 +0600

    update: :hammer: 解

---
+++ leetcode/easy/remove_element/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+
+    def removeElement(self, nums: List[int], val: int) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    nums = [3, 2, 2, 3]
+    val = 3
+
+    solution = Solution()
+    result = solution.removeElement(nums, val)
+
+    print(result)

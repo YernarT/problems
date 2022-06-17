3b6e6ed8cc5acfc4ca5e86a335b511c273f6270c
YernarT <toktaryernar@gmail.com>
2022-06-17 14:12:09 +0600

    update: :hammer: 解

--- leetcode/easy/remove_element/solution.py
+++ leetcode/easy/remove_element/solution.py
@@ -4,7 +4,10 @@ from typing import List
 class Solution:
 
     def removeElement(self, nums: List[int], val: int) -> int:
-        pass
+        while val in nums:
+            nums.remove(val)
+        
+        return len(nums)
 
 
 if __name__ == '__main__':

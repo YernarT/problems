d6423804359d9abab112f8a51a4c4e3b274d756e
YernarT <toktaryernar@gmail.com>
2022-07-03 12:54:27 +0600

    wip: :construction: 未完成

--- leetcode/easy/merge_two_sorted_lists/solution.py
+++ leetcode/easy/merge_two_sorted_lists/solution.py
@@ -6,10 +6,32 @@ class ListNode:
         self.val = val
         self.next = next
 
+# 报错
+'''
+TypeError: object of type 'ListNode' has no len()
+    for i in range(max(len(list1), len(list2))):
+Line 14 in mergeTwoLists (Solution.py)
+    ret = Solution().mergeTwoLists(param_1, param_2)
+Line 47 in _driver (Solution.py)
+    _driver()
+Line 58 in <module> (Solution.py)
+'''
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        pass
-        
+
+        result = []
+
+        for i in range(max(len(list1), len(list2))):
+            list1_el = list1[i]
+            list2_el = list2[i]
+            
+            if list1_el > list2_el:
+                result.extend([list2_el, list1_el])
+            else:
+                result.extend([list1_el, list2_el])
+
+        return result
 
 
 if __name__ == '__main__':
